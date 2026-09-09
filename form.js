74e74d98356908a15838f9740175b51366684076
jscgh <j.caley@unsw.edu.au>
2026-09-09 17:05:02 +1000

    Remove remaining command preview scaffolding

--- form.js
+++ form.js
@@ -62,10 +62,6 @@
       .replace(/\s+/g, "-");
   };
 
-  if (typeof module !== "undefined" && module.exports) {
-    module.exports = { sampleIdForInput };
-  }
-
   const escapeForSelector = (value) => {
     if (window.CSS && typeof window.CSS.escape === "function") {
       return window.CSS.escape(value);
